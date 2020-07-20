53fb033a6533f72c0dbc0ffb44805fbccdf83d1e
anandarafaele <ananda_rafaele@hotmail.com>
2020-07-20 12:28:00 -0300

    Resolução da Questão 16

---
+++ Resolução de Questionários/ExerciciosFundamentos/q16.js
@@ -0,0 +1,21 @@
+function calculadora(n1, op, n2) {
+    switch (op) {
+        case "+":
+            return n1 + n2
+        case "-":
+            return n1 - n2
+        case "*":
+            return n1 * n2
+        case "/":
+            return n1 / n2
+        default:
+            return "Operação inválida"
+    }
+}
+
+
+console.log(calculadora(2, '+', 3));
+console.log(calculadora(2, '-', 3));
+console.log(calculadora(2, '*', 3));
+console.log(calculadora(2, '/', 3));
+console.log(calculadora(2, 'a', 3));
